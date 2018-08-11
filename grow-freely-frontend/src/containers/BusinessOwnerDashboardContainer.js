d02a180fe9c32614a5d79a46b5b3972e96e95f41
liamkillion <liam.killion@gmail.com>
2018-08-11 18:29:38 -0400

    added more containers

---
+++ grow-freely-frontend/src/containers/BusinessOwnerDashboardContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class BusinessOwnerDashboardContainer extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default BusinessOwnerDashboardContainer;
---
+++ grow-freely-frontend/src/containers/BusinessOwnerSurveyContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class BusinessOwnerSurveyContainer extends React.Component {
+  render() {
+
+  }
+}
+
+export default BusinessOwnerSurveyContainer;
