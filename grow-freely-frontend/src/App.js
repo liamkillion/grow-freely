d2163ef80adb3763e26415d6b0cfbfb6a6141598
liamkillion <liam.killion@gmail.com>
2018-08-11 15:52:47 -0400

    add material ui

--- grow-freely-frontend/src/App.js
+++ grow-freely-frontend/src/App.js
@@ -1,19 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, withRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <Switch>
+        <Route
+          path="/landing"
+          render={routerProps => {
+            return <LandingContainer {...routerProps} />;
+          }}
+        />
+        <Route
+          path="/consumer"
+          render={routerProps => {
+            return <ConsumerContainer {...routerProps} />;
+          }}
+        />
+        <Route
+          path="/entrepreneur"
+          render={routerProps => {
+            return <EntrepreneurContainer {...routerProps} />;
+          }}
+        />
+        <Route
+          path="/financial"
+          render={routerProps => {
+            return <FinancialConsumer {...routerProps} />;
+          }}
+        />
+      </Switch>
     );
   }
 }
